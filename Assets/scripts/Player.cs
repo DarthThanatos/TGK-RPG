f19f66e8b14e4a5a00a63d8857a378856ffa7f24
DarthThanatos <robert.bielas@interia.pl>
2018-04-08 17:41:48 +0200

    enemies actually attack the player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public  CharacterStats characterStats;
+    public CharacterStats characterStats;
+    public float currentHealth;
+    public float maxHealth = 100;
 
     void Start()
     {
+        Debug.Log("Setting player health to " + maxHealth);
+        currentHealth = maxHealth;
         characterStats = new CharacterStats(10, 10, 10);
+    }
+
+    public void takeDamage(int amount)
+    {
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            die();
+        }
+    }
+
+
+    private void die()
+    {
+        Debug.Log("Player dead, restoring health of the Nameless One");
+        currentHealth = maxHealth;
     }
 }
--- Assets/scripts/Slime.cs
+++ Assets/scripts/Slime.cs
@@ -1,24 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Slime : MonoBehaviour, IEnemy {
 
-    public float currentHealth, power, thoughness;
-    public float maxHealth;
+
+    public LayerMask aggroLayerMask;
+    private NavMeshAgent navMeshAgent;
+
+    public float currentHealth;
+    public float maxHealth = 100;
 
     private CharacterStats characterStats;
+    private Player player;
 
     void Start()
     {
+        navMeshAgent = GetComponent<NavMeshAgent>();
         characterStats = new CharacterStats(6,10,2);
-
         currentHealth = maxHealth;
+    }
+
+    void FixedUpdate()
+    {
+        Collider[] withinAggroColliders = Physics.OverlapSphere(transform.position, 10, aggroLayerMask);
+        if(withinAggroColliders.Length > 0)
+        {
+            Collider collider = withinAggroColliders[0];
+            ChasePlayer(collider.GetComponent<Player>());
+        }
+    }
+
+    void ChasePlayer(Player player)
+    {
+        this.player = player;
+        navMeshAgent.SetDestination(player.transform.position);
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            if (!IsInvoking("performAttack"))
+                InvokeRepeating("performAttack", .5f, 3f);
+        }
+        else
+        {
+            if (IsInvoking("performAttack"))
+            {
+                CancelInvoke("performAttack");
+            }
+        }
     }
 
     public void performAttack()
     {
-        throw new System.NotImplementedException();
+        if(player != null)
+        {
+            player.takeDamage(5);
+        }
     }
 
     public void takeDamage(int amount)
